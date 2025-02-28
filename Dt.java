1f6fd3a40197c35dc3f8deff9536d6064f4f01b3
sehernuryildiz <sehernury2@gmail.com>
2025-02-28 12:14:30 +0300

    Add files via upload

---
+++ Dt.java
@@ -0,0 +1,19 @@
+public class Dt {
+
+    public static void main(String[] args) {
+
+        int grade = 1;
+        double grade2 = 1.5;
+        boolean isok = true;
+        char first = 's';
+
+        System.out.println(grade);
+        System.out.println(grade2);
+        System.out.println(isok);
+        System.out.println(first);
+
+
+
+
+    }
+}
